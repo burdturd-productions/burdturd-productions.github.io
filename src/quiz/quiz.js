75280491e1bd384e59b0e890b03e7306913971ca
Angela Vawser <amvawser@gmail.com>
2019-08-07 16:46:39 -0700

    udpates to css and wrote out quiz.js

--- src/quiz/quiz.js
+++ src/quiz/quiz.js
@@ -1,6 +1,37 @@
 import store from '../store.js';
 
 // Get user inputs captured as a variable
+
+const choiceOne = document.getElementById('choice-one');
+const choiceTwo = document.getElementById('choice-two');
+const choiceThree = document.getElementById('choice-three');
+
 // Assign that a number value
 // updateScore(username, score)
 // throw user to results page.
+
+let userName = store.getCurrentUser();
+let currentUser = store.getUser(userName);
+console.log(userName);
+
+choiceOne.addEventListener('click', ()=> {
+    event.preventDefault();
+    userName.score += 1;
+    store.save('current-user', currentUser);
+    console.log(currentUser.score);
+    window.location = 'fin.html';
+});
+
+choiceTwo.addEventListener('click', ()=> {
+    event.preventDefault();
+    userName.score += 1;
+    store.save('current-user', currentUser);
+    window.location = 'fin.html';
+});
+
+choiceThree.addEventListener('click', ()=> {
+    event.preventDefault();
+    userName.score += 0;
+    store.save('current-user', currentUser);
+    window.location = 'fin.html';
+});
