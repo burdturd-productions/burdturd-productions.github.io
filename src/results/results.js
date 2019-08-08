51a8f027168b40f638846f136b5f802bcbea061c
JOHN NELSON <JohnNelson4850@gmail.com>
2019-08-08 13:38:07 -0700

    3 results

--- src/results/results.js
+++ src/results/results.js
@@ -8,13 +8,18 @@ let currentUser = store.getCurrentUser();
 let userScore = currentUser.score;
 console.log(userScore);
 
-// Need and if statement that Renders results based on score.
-if(userScore > 0) {
+// Need an if statement that Renders results based on score.
+if(userScore === 2) {
     sideImg.src = '../../assets/character-right.png';
     storyImg.src = '../../assets/wtfbirb.jpg';
-    textResult.textContent = 'You Fucking Did It...Win!';
+    textResult.textContent = 'Georgie O. Squirrel was right, it WAS Aliens!';
 }
-else {
+else if(userScore === 1) {
+    sideImg.src = '../../assets/character-right.png';
+    storyImg.src = '../../assets/placeholder-pigeon.jpg';
+    textResult.textContent = 'Something doesn\'t add up, are you sure you solved the case?' ;
+}
+else if(userScore === 0) {
     sideImg.src = '../../assets/character-right.png';
     storyImg.src = '../../assets/placeholder-pigeon.jpg';
     textResult.textContent = 'You Were Terrible...Don\'t Even Bother Trying Again...Loser!';
